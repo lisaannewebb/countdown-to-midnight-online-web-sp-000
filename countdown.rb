328fc51c158a7ab44a12a056677ff81f299e2c2e
Lisa Webb <lwebbnyc@outlook.com>
2019-03-08 17:47:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,14 @@
 #write your code here
 
 def countdown
-  #{number} SECOND(S)!
+  number = 1 
   
-  "HAPPY NEW YEAR!"
+    while number < 10 
+      puts "#{number} SECOND(S)!"
+      
+      number -= 1
+      
+      puts "HAPPY NEW YEAR!"
 
 
 end
