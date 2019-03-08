f2762f04cfae8381f8488e5ac684e48ffce153ee
Lisa Webb <lwebbnyc@outlook.com>
2019-03-08 17:51:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,15 +3,8 @@
 def countdown(seconds)
     while seconds >0
       puts "#{seconds} SECOND(S)!"
-      count-=1
+      seconds-=1
     end
     "HAPPY NEW YEAR!"
   end
 
-  def countdown_with_sleep(count)
-    while count > 0
-      count-=1
-      sleep(1)
-    end
-      puts "0 left. HAPPY NEW YEAR!"
-  end
